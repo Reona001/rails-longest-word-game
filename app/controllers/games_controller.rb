2d4fdabe8c7a9f1a20cb0dac0ef0d5ec915775b2
reona kasuya <reon.kasuya@gmail.com>
2022-01-07 09:04:06 +0900

    API in progress

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,6 +5,7 @@ class GamesController < ApplicationController
 
   def score
     @input = params[:input]
-    raise
   end
 end
+
+# {"message":"welcome","endpoints":["https://wagon-dictionary.herokuapp.com/:word","https://wagon-dictionary.herokuapp.com/autocomplete/:stem"],"total_api_hits":4663005,"words_found":2514548,"autocomplete_hits":1210464}
